8e47094daadb6c05c0a0d315f1e2ef051d6633b1
Daniel Clain <daniel.clain@hotmail.com>
2019-01-20 15:36:29 +0000

    dockerfile test

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,8 @@
-FROM nginx:alpine
+
+FROM nginx:1.13.3-alpine
+## Remove default nginx website
+RUN rm -rf /usr/share/nginx/html/*
+## From 'builder' stage copy over the artifacts in dist folder to default nginx public folder
+COPY /dist/PersonalQuiz /usr/share/nginx/html
+EXPOSE 80
+CMD ["nginx", "-g", "daemon off;"]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyByoHAvuRSgSEa0cnVyX1D9KbcS_rQXoc4",
+    authDomain: "personal-quiz-54618.firebaseapp.com",
+    databaseURL: "https://personal-quiz-54618.firebaseio.com",
+    projectId: "personal-quiz-54618",
+    storageBucket: "personal-quiz-54618.appspot.com",
+    messagingSenderId: "442724384951"
+  },
 };
