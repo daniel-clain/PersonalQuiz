370540e9343eb1cb4bc7174a703398c325dc2efc
Daniel Clain <daniel.clain@hotmail.com>
2019-02-16 09:50:06 +0000

    update docker add new demo env

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,4 @@
-
-FROM nginx:1.13.3-alpine
-## Remove default nginx website
-RUN rm -rf /usr/share/nginx/html/*
-## From 'builder' stage copy over the artifacts in dist folder to default nginx public folder
-COPY /dist/PersonalQuiz /usr/share/nginx/html
+FROM nginx:alpine
+COPY ./dist/PersonalQuiz /usr/share/nginx/html
 EXPOSE 80
-CMD ["nginx", "-g", "daemon off;"]
+ENTRYPOINT ["nginx","-g","daemon off;"]
---
+++ src/environments/environment.demo.ts
@@ -0,0 +1,13 @@
+
+export const environment = {
+	production: true,
+	firebase: {
+		apiKey: "AIzaSyDRDH7B3pafOhb8oRBOo8C-1qY-c7MMey8",
+    authDomain: "personalquizdemo-b770d.firebaseapp.com",
+    databaseURL: "https://personalquizdemo-b770d.firebaseio.com",
+    projectId: "personalquizdemo-b770d",
+    storageBucket: "personalquizdemo-b770d.appspot.com",
+    messagingSenderId: "939167307939"
+	},
+};
+  
