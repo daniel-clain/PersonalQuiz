ad821ad89195ffccb0d2d696dfec657d6daf9fae
Daniel Clain <daniel.clain@talktalkplc.com>
2019-03-06 12:10:46 +0000

    update package lock

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyByoHAvuRSgSEa0cnVyX1D9KbcS_rQXoc4",
-    authDomain: "personal-quiz-54618.firebaseapp.com",
-    databaseURL: "https://personal-quiz-54618.firebaseio.com",
-    projectId: "personal-quiz-54618",
-    storageBucket: "personal-quiz-54618.appspot.com",
-    messagingSenderId: "442724384951"
+    apiKey: 'AIzaSyByoHAvuRSgSEa0cnVyX1D9KbcS_rQXoc4',
+    authDomain: 'personal-quiz-54618.firebaseapp.com',
+    databaseURL: 'https://personal-quiz-54618.firebaseio.com',
+    projectId: 'personal-quiz-54618',
+    storageBucket: 'personal-quiz-54618.appspot.com',
+    messagingSenderId: '442724384951'
   },
 };
 
